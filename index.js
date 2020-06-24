6d818f9562b46d718408ff3944c4c28dbdb43099
jeeveshkataria <jeeveshkataria92@gmail.com>
2020-06-25 02:50:55 +0530

    second commit

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require('express');
-const app     = express(); 
+const app = express();
 
-app.get('/',
-    (req,res) => {
-        res.send({hi : 'there'});
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' });
+});
 
-    });
-
-const PORT = process.env.PORT || 5002 ;
-app.listen(5002);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT,'0.0.0.0');
