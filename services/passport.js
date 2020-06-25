90e49a29a490f745151cf5f357d174f84a9ec7c5
jeeveshkataria <jeeveshkataria92@gmail.com>
2020-06-26 02:03:21 +0530

    sec4

--- services/passport.js
+++ services/passport.js
@@ -7,6 +7,28 @@ const User = mongoose.model('users');
 //one argument means : we are fetching from it
 //two arguments : we are loading from it
 
+
+passport.serializeUser( ( user , done ) => {
+  done( null , user.id );
+  // user.id recognise user by id assigned by mongo
+  //reason why we are not using profile id? bz profile id is 
+  // unique to google id, but user id allocated to google,fb logon
+});
+
+
+passport.deserializeUser( ( id , done ) => {
+// return the mongo id
+  User.findById(id)
+    .then(user => {
+      done(null,user);
+    });
+});
+
+
+
+
+
+
 passport.use(
     new GoogleStrategy(
       {
@@ -15,16 +37,22 @@ passport.use(
         callbackURL: '/auth/google/callback'
       },
       (accessToken , refreshToken , profile , done ) => {
-        User.findOne({ googleID : profile.id  })
-          .then((existingUser) => {
+        User.findOne({ googleID : profile.id  }).then( existingUser => {
             if(existingUser)
             {
+
               //we have record with profile id
+              done( null , existingUser );
+              //tells passport about user
             } 
             else
             {
               // we dont have a user with profile id
-              new User({ googleID : profile.id }).save();
+              new User({ googleID : profile.id })
+              .save()
+              .then( user => done( null , user));
+
+
             }
 
           }
