b464e922ba04664bebde73245c17e1aaa760a30e
jeeveshkataria <jeeveshkataria92@gmail.com>
2020-06-26 23:37:45 +0530

    sess_6

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          {/* Edit <code>src/App.js</code> and save to reload. */}
+          hi,there
+
+        </p>
+        <a
+        href="/auth/google"
+        >
+          Sign In With Google
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
