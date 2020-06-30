85c01b82584dd12daf2485fc2dad2315abe8bc02
jeeveshkataria <jeeveshkataria92@gmail.com>
2020-06-30 22:10:57 +0530

    161

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -3,10 +3,13 @@
 import React from 'react';
 
 
-export default ({ input }) => {
+export default ({ input , label }) => {
   
     return (
         <div>
+            <label>
+               {label} 
+            </label>
             <input  {...input}  />
         </div>
     );
