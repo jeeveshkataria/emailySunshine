25cba384607ac9bdc0ee5159fb7de115b9b92bd1
jeeveshkataria <jeeveshkataria92@gmail.com>
2020-06-30 17:13:31 +0530

    151

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -6,6 +6,14 @@ const Dashboard = () => {
     return (
         <div>
             Dashboard
+            <div className = "fixed-action-btn">
+                <a className = "btn-floating btn-large red">
+                   <i className = "material-icons">
+                       add    
+                    </i> 
+                </a>
+            </div>
+
         </div>
     );
 };
