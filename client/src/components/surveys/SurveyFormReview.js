00b269c40534f34652708f49621d8841ea079809
jeeveshkataria <jeeveshkataria92@gmail.com>
2020-07-01 02:49:41 +0530

    171

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,11 +1,17 @@
+// survey form review show users about their review
+
+
 import React from 'react';
 
-const SurveyReview = () => {
+const SurveyFormReview = ({ onCancel }) => {
     return (
         <div>
             <h5>Please confirm your Entries:</h5>
+            <button className = "yellow darken-3 btn-flat" onClick = {onCancel}>
+            Back
+            </button>
         </div>
     );
 };
 
-export default SurveyReview;
+export default SurveyFormReview;
