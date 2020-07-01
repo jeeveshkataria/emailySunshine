cc0faec8be6e12e28f7dcf07618ca7bca9b2ac7a
jeeveshkataria <jeeveshkataria92@gmail.com>
2020-07-01 17:18:48 +0530

    187

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ require('./services/passport');
 
 
 
+// mongoose.connect(keys.mongoURI , { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connect(keys.mongoURI);
-
 
 const app = express();
 
