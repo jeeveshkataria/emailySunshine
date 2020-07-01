92a9d2d520962529b4dd90b26a61bce5aceb8ed4
jeeveshkataria <jeeveshkataria92@gmail.com>
2020-07-01 20:09:08 +0530

    193

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,9 +1,11 @@
+const _     = require('lodash');
+const Path  = require('path-parser'); 
+const { URL }  = require('url');
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 const Mailer = require('../services/Mailer');
 const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
-
 const Survey = mongoose.model('surveys');
 
 
@@ -14,8 +16,20 @@ module.exports = app => {
     });
 
     app.post('/api/surveys/webhooks',( req , res ) => {
-        console.log(req.body);
-        res.send({  });
+        const events = _.map(req.body , ({ email, url }) => {
+        const pathname =  new URL(url).pathname;  
+        const p = new Path('/api/surveys/:surveyId/:choice');
+        const match = p.test(pathname);
+        if(match){
+            return {  email, surveyId: match.surveyId,  choice: match.choice };
+        }
+        });
+        // remove undefined events
+        const compactEvents = _.compact(events);
+        // Now, remove duplicate
+        const uniqueEvents = _.uniqBy( compactEvents , 'email' , 'surveyId');
+
+        console.log(uniqueEvents);
     });
 
 
